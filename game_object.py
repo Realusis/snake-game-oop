824529ee27f860bb82abfbd550d00676f46f7979
RealUsis <100910819+RealTautvis@users.noreply.github.com>
2026-04-14 21:00:34 +0300

    Create game_object.py
    Add GameObject base class

---
+++ game_object.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class gameobject(ABC):
+    """Abstract base class for all game objects apples, snake etc."""
+    
+    def __init__(self):
+        self._x = x
+        self._y = yield
+
+    @roperty
+    def x(self):
+        return self._x
+    
+    @property
+    def y(self):
+        return self._y
+    
+    @abstractmethod
+    def draw(self, screen):
+        pass
